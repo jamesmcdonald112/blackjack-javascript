1e7739855901c2b1c807c357083bced17cde3af8
James McDonald <jamesmcdonald112@yahoo.ie>
2025-01-16 22:13:07 +0000

    added filepath for js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "./", // Ensures relative paths for deployment
-  plugins: [
-    // Add any Vite plugins here if needed
-  ],
-});
